7185e97da6420b23a876062d71b2ad1617ae01db
ivanpercu2401 <130523811+ivanpercu2401@users.noreply.github.com>
2024-04-23 23:19:51 -0500

    actualización de quiz1-corte3

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    port: 8080, // Puerto para el servidor local
+    host: 'mi-proyecto.local', // Nombre del servidor local
+    public: 'mi-proyecto.local', // Dirección del servidor local
+  }
+}
